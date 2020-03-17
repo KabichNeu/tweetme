6dececca9bf77c6e5dbd8a714c2a33d8ce8f29fc
kabichneu <kabichneu@gmail.com>
2020-03-17 09:35:45 +0545

    after adding user customization models

--- tweetme/accounts/models.py
+++ tweetme/accounts/models.py
@@ -1,43 +1,60 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.backends import BaseBackend
-from django.contrib.auth.hashers import check_password
 from django.contrib.auth.models import User
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import (AbstractUser,BaseUserManager)
 
 
-"""Writing user authentication backend"""
-class SettingsBackend(BaseBackend):
 
-    def authenticate(self, request, username = None, password= None):
-        login_valid = (settings.ADMIN_LOGIN == username)
-        pwd_valid = check_password(password,settings.ADMIN_PASSWORD)
-        if login_valid and pwd_valid:
-            try:
-                user = User.objects.get(username = username)
-            except User.DoesNotExist:
-                user = User(username = username)
-                user.is_staff = True
-                user.is_superuser = True
-                user.save()
+
+class MyUserManager(BaseUserManager):
+    def create_user(self,email,date_of_birth,password=None):
+        if not email:
+            raise ValueError('Users must have ann email address')
+        user = self.model(
+            email = self.normalize_email(email),
+            date_of_birth = date_of_birth,
+        )
+        user.set_password(password)
+        user.save(using = self._db)
+        return user
+
+        def create_superuser(self,email,date_of_birth,password = None):
+            user = self.create_user(email,password = password,date_of_birth=date_of_birth,)
+            user.is_admin = True
+            user.save(using = self._db)
             return user
-        return None
+        
+class MyUser:
+    email = models.EmailField(
+        verbose_name = "email address",
+        max_length = 255,
+        unique = True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default = True)
+    is_admin = models.BooleanField(default = False)
+    
+    objects = MyUserManager()
 
-    def get_user(self,user_id):
-        try:
-            return User.objects.get(pk = user_id)
-        except User.DoesNotExist:
-            return None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
 
-"""Handling authorization in custom backends"""
-class MagicAdminBackend(BaseBackend):
-    def has_perm(self,user_obj,perm,obj=None):
-        return user_obj.username == settings.ADMIN_LOGIN
+    def __str__(self):
+        return self.email
 
-
-
-class Employee(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    department = models.CharField(max_length = 100)
+    def has_perm(self,perm,obj = None):
+        """For users specific permission"""
+        return True
+    
+    def has_module_perms(self,app_label):
+        """ users --> to have a permission to view the app 'app label' """
+        return True
+    
+    @property
+    def is_staff(self):
+        """Is the user a member of staff?"""
+        """alll admins are staff"""
+        return self.is_admin
+    
 
 
